4dbd73162f28ea9eacfc8baf30ca7f58ad5685d1
guodongdong <dd.guo@foxmail.com>
2013-09-11 20:56:51 +0800

    first add

---
+++ network.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.6
+# encoding: utf-8
+# Author: guodongdong <dd.guo@foxmail.com>
+# Created Time: 2013年09月11日 星期三 20时56分44秒
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
