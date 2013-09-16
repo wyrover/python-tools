62d8e6b6e363bcbc87e604e9cdc8fa7c57cc304c
guodongdong <dd.guo@foxmail.com>
2013-09-16 14:27:40 +0800

    add get_redirected

--- url.py
+++ url.py
@@ -2,6 +2,14 @@
 # encoding: utf-8
 # Author: guodongdong <dd.guo@foxmail.com>
 # Created Time: 2013年09月11日 星期三 20时55分00秒
+import urllib2
+import cookielib
+
+def get_redirected(url):
+    jar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+    f = opener.open(url)
+    return f.geturl()
 
 def main():
     pass
