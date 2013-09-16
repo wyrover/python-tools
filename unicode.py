f181cf971da8eee54771050ab61acac10307fe65
guodongdong <dd.guo@foxmail.com>
2013-09-16 15:41:55 +0800

    add unicode and url tools

--- unicode.py
+++ unicode.py
@@ -3,6 +3,37 @@
 # Author: guodongdong <dd.guo@foxmail.com>
 # Created Time: 2013年09月11日 星期三 20时55分15秒
 
+def str_to_unicode(text, encoding=None, errors='strict'):
+    """Return the unicode representation of text in the given encoding. Unlike
+    .encode(encoding) this function can be applied directly to a unicode
+    object without the risk of double-decoding problems (which can happen if
+    you don't use the default 'ascii' encoding)
+    """
+    
+    if encoding is None:
+        encoding = 'utf-8'
+    if isinstance(text, str):
+        return text.decode(encoding, errors)
+    elif isinstance(text, unicode):
+        return text
+    else:
+        raise TypeError('str_to_unicode must receive a str or unicode object, got %s' % type(text).__name__)
+
+def unicode_to_str(text, encoding=None, errors='strict'):
+    """Return the str representation of text in the given encoding. Unlike
+    .encode(encoding) this function can be applied directly to a str
+    object without the risk of double-decoding problems (which can happen if
+    you don't use the default 'ascii' encoding)
+    """
+
+    if encoding is None:
+        encoding = 'utf-8'
+    if isinstance(text, unicode):
+        return text.encode(encoding, errors)
+    elif isinstance(text, str):
+        return text
+    else:
+        raise TypeError('unicode_to_str must receive a unicode or str object, got %s' % type(text).__name__)
 def main():
     pass
 
--- url.py
+++ url.py
@@ -2,14 +2,74 @@
 # encoding: utf-8
 # Author: guodongdong <dd.guo@foxmail.com>
 # Created Time: 2013年09月11日 星期三 20时55分00秒
-import urllib2
 import cookielib
+import re
+import types
+import urllib2
+import urlparse
+
+import hashlib
+
+
+from unicode import unicode_to_str
+from libs.TLDs.regDomain import getRegisteredDomain
+from libs.TLDs.effectiveTLDs import tldTree
+
+ip_pattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+
+def get_domain(url):
+
+    host = urlparse.urlparse(url).hostname
+    domain = host
+
+    if host:
+        #ip
+        if ip_pattern.match(host):
+            domain = host
+        else:
+            """
+                use 3rd lib to parse domain
+            """
+            domain = getRegisteredDomain(host, tldTree)
+    return domain
 
 def get_redirected(url):
     jar = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
     f = opener.open(url)
     return f.geturl()
+
+def get_uid(url):
+    """
+        get the uid of the url
+        algorithm:
+        1) get 16 bytes (128 bits) md5, encoded by hex
+        2) split the first 8 bytes and the last 8 bytes
+        3) convert the two 8 bytes into int
+        4) XOR the two 8 bytes
+        5) encode the result by hex
+    """
+    # convert unicode to str (with encode utf-8)
+    # this function is str safe, without double encode error
+    url = unicode_to_str(url)
+    
+    if isinstance(url, types.StringType):
+        # md5 is a string represents a 32bytes hex number
+        md5 = hashlib.new("md5", url).hexdigest()
+        first_half_bytes = md5[:16]
+        last_half_bytes = md5[16:]
+        
+        # get the two long int
+        first_half_int = int(first_half_bytes, 16)
+        last_half_int = int(last_half_bytes, 16)
+        
+        # XOR the two long int, get a long int
+        xor_int = first_half_int ^ last_half_int
+        
+        # convert to a hex string
+        uid = "%x" % xor_int
+        
+        return uid
 
 def main():
     pass
